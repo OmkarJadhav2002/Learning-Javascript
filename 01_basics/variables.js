4045409c6316d69f8d10e975d45a56693e6bfe28
Omkar Santosh Jadhav <106821060+OmkarJadhav2002@users.noreply.github.com>
2023-10-02 15:31:58 +0000

    variables in js

---
+++ 01_basics/variables.js
@@ -0,0 +1,23 @@
+
+// Variables and Constants in JS 
+
+// understand with some real-time scenario (user login/registered) 
+const accountId = 123 
+let accountEmail = "omkar23@gmail.com" 
+var accountPassword = "321" 
+accountCity = "Sangamner" 
+let accountState;
+
+// accountId = 2  // cannot change the value/not allowed 
+accountEmail = "omi@gmai.com" // allowed 
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) 
+
+{
+    // scope 
+}
+
+/*
+prefered not to use `var` because of issue in block scope and functional scope 
+*/
