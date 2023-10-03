c976dcda7c9cd03add637e225ce4f4ba2e2d6836
Omkar Santosh Jadhav <106821060+OmkarJadhav2002@users.noreply.github.com>
2023-10-03 17:36:40 +0000

    string(dtype) basics

---
+++ 01_basics/strings.js
@@ -0,0 +1,10 @@
+// Strings in JS 
+
+const name = "Omkar" 
+const repoCount = 15 
+
+// console.log(name + repoCount) // outdated syntax 
+
+// Another way: string interpolation, use back ticks with $ sign followed by {}
+// more readable, reliable, mordern way 
+console.log(`My name is ${name}. I have created about ${repoCount} repositories on gitgub.`) 
