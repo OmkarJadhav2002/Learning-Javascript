87b7e8f3a616d11ae8d97e38b9eecc96365e6bd0
Omkar Santosh Jadhav <106821060+OmkarJadhav2002@users.noreply.github.com>
2023-10-05 17:55:44 +0000

    string functions

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -8,3 +8,12 @@ const repoCount = 15
 // Another way: string interpolation, use back ticks with $ sign followed by {}
 // more readable, reliable, mordern way 
 console.log(`My name is ${name}. I have created about ${repoCount} repositories on gitgub.`) 
+
+// another syntax to define the string 
+const carName = new String("Porsche Macan")
+console.log(carName) 
+console.log(carName[1]) 
+console.log(carName.length)
+console.log(carName.toUpperCase()) 
+console.log(carName.charAt(2)) 
+console.log(carName.indexOf('o')) 
